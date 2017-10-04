db21aad33d97e086be0e2ded883e388c0cd0c69c
HassanQbatch <hassan.tariq@qbatch.com>
2017-10-04 17:12:10 +0500

    Skip CSRF Validation

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,4 +1,7 @@
 class PointsController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
   def create
     puts "HI"
     if params[:data].present?
