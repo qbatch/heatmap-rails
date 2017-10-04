d165c588f03a101bb1288ec0544baad4303c3115
HassanQbatch <hassan.tariq@qbatch.com>
2017-10-04 13:00:20 +0500

    Heat Map Model And Points Controller Added

---
+++ app/controllers/points_controller.rb
@@ -0,0 +1,12 @@
+class PointsController < ApplicationController
+  def create
+    if params[:data].present?
+      for i in 0..2
+        HeatMap.create(path: params[:data]["#{i}"][:path], x_coordinate: params[:data]["#{i}"][:x], y_coordinate: params[:data]["#{i}"][:y], value: 5, click_type: 'click')
+      end
+    end
+    if params[:type] == 'move'
+      HeatMap.create(path: params[:path], x_coordinate: params[:x], y_coordinate: params[:y], value: 5, click_type: 'move')
+    end
+  end
+end
---
+++ app/models/heat_map.rb
@@ -0,0 +1,2 @@
+class HeatMap < ApplicationRecord
+end
