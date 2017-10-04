ae24bad51c6909866e54ae36092dc18b02126f2c
HassanQbatch <hassan.tariq@qbatch.com>
2017-10-04 12:15:52 +0500

    Generator dependency

--- heatmap_rb.gemspec
+++ heatmap_rb.gemspec
@@ -31,4 +31,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16.a"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "generator_spec", "~> 3.0"
 end
--- lib/generators/heatmap_rb/install_generator.rb
+++ lib/generators/heatmap_rb/install_generator.rb
@@ -4,20 +4,15 @@ require 'rails/generators/base'
 
 class HeatmapRb
   class InstallGenerator < Rails::Generators::Base
-    # include Rails::Generators::Migration
-    # source_root File.expand_path('../templates', __FILE__)
+    include Rails::Generators::Migration
+    source_root File.expand_path('../templates', __FILE__)
 
-    # def self.next_migration_number(path)
-    #   Time.now.utc.strftime("%Y%m%d%H%M%S")
-    # end
-
-    # def create_model_file
-    #   migration_template "initializer.rb", "db/migrate/add_foo_to_bar.rb"
-    # end
-
-    def test
-      puts "Hi"
+    def self.next_migration_number(path)
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
     end
 
+    def create_model_file
+      migration_template "initializer.rb", "db/migrate/add_foo_to_bar.rb"
+    end
   end
 end
