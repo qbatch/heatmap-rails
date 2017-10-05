5043265a4ba823a7691be6baa6a658169d2f09e7
HassanQbatch <hassan.tariq@qbatch.com>
2017-10-05 13:20:06 +0500

    Helper Testing

--- lib/heatmap_rb/helper.rb
+++ lib/heatmap_rb/helper.rb
@@ -2,8 +2,9 @@ module HeatmapRb
   module Helper
 
     def save_heatmap(options = {})
-
-
+      puts "*******************************"
+      html = "<div></div>"
+      html.respond_to?(:html_safe) ? html.html_safe : html
     end
 
   end
