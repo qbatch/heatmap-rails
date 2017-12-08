31512e46294cf2fb97d7477d6d97ce65569ac591
Awais Tahir <awais.tahir@qbatch.com>
2017-12-08 11:05:35 +0500

    Code Refactoring

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,18 +3,14 @@ class PointsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    if params[:click_data].present?
-      if params[:total_clicks].to_i > 0
-        for i in 0..params[:total_clicks].to_i-1
-           HeatMap.create(path: params[:click_data]["#{i}"][:path], click_type: 'click',xpath: params[:click_data]["#{i}"][:xpath])
-        end
+    if params[:click_data].present? && params[:total_clicks].to_i > 0
+      for i in 0..params[:total_clicks].to_i-1
+         HeatMap.create(path: params[:click_data]["#{i}"][:path], click_type: 'click',xpath: params[:click_data]["#{i}"][:xpath])
       end
     end
-    if params[:move_data].present?
-      if params[:total_moves].to_i > 0
-        for i in 0..params[:total_moves].to_i-1
-          HeatMap.create(path: params[:move_data]["#{i}"][:path], click_type: 'move',xpath: params[:move_data]["#{i}"][:xpath])
-        end
+    if params[:move_data].present? && params[:total_moves].to_i > 0
+      for i in 0..params[:total_moves].to_i-1
+        HeatMap.create(path: params[:move_data]["#{i}"][:path], click_type: 'move',xpath: params[:move_data]["#{i}"][:xpath])
       end
     end
   end
